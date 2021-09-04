8f238b6fe3d6e55e30e5fc7b341c3101e744770b
IslemPHP <islem-amm@hotmail.com>
2021-09-04 18:52:12 +0100

    create projet

---
+++ flutter_book/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'screens/listBookScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Book Task',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ListBookScreen(title: 'List Books'),
+    );
+  }
+}
